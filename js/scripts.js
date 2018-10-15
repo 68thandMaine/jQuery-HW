89f53242fb33b3008a826bbd679c3e54cde7777d
Chris Rudnicky and Ngan Nguyen <ngan@>
2018-10-15 15:58:23 -0700

    Add javascript jQuery elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,13 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
-};
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-var multiply = function(number1, number2) {
-  return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-  return number1 / number2;
-};
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply (number1, number2);
-alert(result);
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
+});
