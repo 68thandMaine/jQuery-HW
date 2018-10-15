589c1f845d18dca3977dfea484e57526f48ba0b7
Chris Rudnicky and Ngan Nguyen <chris@>
2018-10-15 16:47:55 -0700

    Added some script edits

---
+++ js/cipher.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+var text = function(speak) {
+  return speak ;
+}
+var speak = prompt("Write a four letter word please");
+var result = text(speak);
+// // alert(result);
+//
+// var first = function(result){
+//   return result.toUpperCase().charAt(0);
+// }
+
+// var total = function(result) {
+//   return result.length;
+// }
+// var last = function(result){
+//   return result.toUpperCase().charAt(total);
+// }
+
+// newResult.length;
+
+speak.split(" ");
+console.log(splitSTring)
+
+
+var newResult = first(result)
+alert(newResult);
+});
